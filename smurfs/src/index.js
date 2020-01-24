dbb259db1ca554566e0c8cc99540eed8de0554b0
Sunil Karki <karkisunil1200@gmail.com>
2020-01-24 10:12:52 -0600

    Created a store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {reducer} from './reducers/';
+import './index.css';
+import App from './components/App';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(<App />, document.getElementById('root'));
